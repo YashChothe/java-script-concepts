52804f3251438afccc6625c1343b66439537a503
YashChothe <119787982+YashChothe@users.noreply.github.com>
2024-03-11 07:51:49 +0000

    changed

---
+++ 01.Basic/pratice.js
@@ -0,0 +1,71 @@
+// variables 
+
+const id = 123355;
+let user = "yash";
+var name= "chothe";
+name2= "abhi";
+
+
+// id= 3456;
+user= "abhi";
+name= "vsgsf";
+name2= "yash";
+
+// console.table([id,user,name,name2]);
+
+// datatypes
+
+// let ary= new Array(1,2,3,4,5);
+// console.log(ary)
+
+// let ary= new Array[(1,2,3,4,50)];// not a syntax
+// // console.log(ary)
+let a= {
+    name:"yash",
+    id: 12435
+}
+// console.log(a);
+
+// let fun= function func() {
+//     console.log(`my name is ${a.name}`);
+    
+// }
+// func();
+//  function func() {
+//     console.log(`my name is ${a.name}`);
+    
+// }
+// func();
+
+let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log( typeof (1 + 2 + "2"));
+// console.log(+true);
+// console.log(+"");
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(3));
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+
+let newDate = new Date()
+console.log(newDate.toLocaleString());
---
+++ 02.Basic/pratice.js
@@ -0,0 +1,16 @@
+// const myArray=[0,1,2,3,4,5];
+// const myHeros=["shaktiman","naagraj"];
+// // const newArry= myArray.join();
+// // console.log ( typeof newArry);
+// const newArry= myHeros.join();
+// console.log (newArry);
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
